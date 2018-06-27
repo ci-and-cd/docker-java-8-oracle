f48ce13c9b77c15631ae3eb11852052e4dfbc98a
haolun <haolun@example.com>
2018-06-28 03:12:41 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ IMAGE_NAME=${IMAGE_PREFIX:-cirepo}/java-8-oracle
 IMAGE_TAG=8u${IMAGE_ARG_JAVA8_VERSION_MINOR}
 if [ "${TRAVIS_BRANCH}" != "master" ]; then IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
-docker build -t \
+docker build \
     --build-arg IMAGE_ARG_JAVA8_VERSION_MINOR=${IMAGE_ARG_JAVA8_VERSION_MINOR:-171} \
     --build-arg IMAGE_ARG_JAVA8_PACKAGE_DIGEST=${IMAGE_ARG_JAVA8_PACKAGE_DIGEST:-512cd62ec5174c3487ac17c61aaa89e8} \
-    ${IMAGE_NAME}:${IMAGE_TAG} .
+    -t ${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${IMAGE_NAME}:${IMAGE_TAG}
 
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${IMAGE_TAG}" == "${IMAGE_TAG_LATEST}" ]; then
