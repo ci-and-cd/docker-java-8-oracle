e3055866f4cf4c24d6dc110f8141a00b1815d055
haolun <haolun@example.com>
2018-07-05 02:04:48 +0800

    update build scripts

--- build.sh
+++ build.sh
@@ -7,18 +7,28 @@ docker-compose version
 
 WORK_DIR=$(pwd)
 
-if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then
-    echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io
-fi
+#if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
-IMAGE_NAME=${IMAGE_PREFIX:-cirepo}/java-8-oracle
+export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
+export IMAGE_NAME=${IMAGE_NAME:-java-8-oracle}
 export IMAGE_TAG=8u${IMAGE_ARG_JAVA8_VERSION_MINOR:-171}
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
-docker-compose build
-docker-compose push
-
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${IMAGE_TAG}" == "${IMAGE_TAG_LATEST}" ]; then
-    docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
-    docker push ${IMAGE_NAME}:latest
+# Build image
+if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+    docker-compose build image
 fi
+
+## Build dumper image
+docker save ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} > dumper/docker/image.tar
+docker-compose build dumper
+
+## Build archive image
+docker-compose build archive
+
+#docker-compose push image
+#if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${IMAGE_TAG}" == "${IMAGE_TAG_LATEST}" ]; then
+#    docker tag ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_PREFIX}/${IMAGE_NAME}:latest
+#    docker push ${IMAGE_PREFIX}/${IMAGE_NAME}:latest
+#fi
+#docker-compose push archive
